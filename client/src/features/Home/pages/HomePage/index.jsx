9c6deb947a7aeddbefc186aebfd77307af8f90c8
ngvuq11 <ngvuquoc.17@gmail.com>
2022-04-07 14:53:21 +0700

    feat: 🎸 status bar

--- client/src/features/Home/pages/HomePage/index.jsx
+++ client/src/features/Home/pages/HomePage/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 function HomePage(props) {
-   return (
-      <div>
-         <h1>Hello 123</h1>
-      </div>
-   );
+  return (
+    <div>
+      <h1>Hello Devplus</h1>
+      <h1>Hello Devplus</h1>
+    </div>
+  );
 }
 
 export default HomePage;
