1f93634b300dfc8ddd9e693caf12ed3d2fb702b6
ngvuq11 <ngvuquoc.17@gmail.com>
2022-04-08 14:27:16 +0700

    feat: 🎸 implement navigation-bar

--- client/src/components/NavigationBar/index.jsx
+++ client/src/components/NavigationBar/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {} from 'antd';
 import './NavigationBar.scss';
 import Container from '../../layouts/Container';
+import SearchForm from '../../features/Home/components/SearchForm';
+import Button from '../../components/Button';
 import Logo from '../Logo';
 // import PropTypes from 'prop-types';
 
@@ -13,6 +15,14 @@ function NavigationBar(props) {
          <Container>
             <div className='navigation__top'>
                <Logo />
+               <SearchForm />
+               <div className='navigation__top--inner'>
+                  <Button className='btn btn__dot btn__white' title='Help' />
+                  <Button
+                     className='btn btn__purple btn__login'
+                     title='Login/Sign'
+                  />
+               </div>
             </div>
             <div className='navigation__bootom'></div>
          </Container>
--- client/src/features/Home/components/SearchForm/index.jsx
+++ client/src/features/Home/components/SearchForm/index.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { Select, Input } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import './SearchForm.scss';
+
+const { Option } = Select;
 
 SearchForm.propTypes = {};
 
 function SearchForm(props) {
-   return <div className='search'></div>;
+   return (
+      <div className='search'>
+         <Select
+            className='search__dropdown'
+            defaultValue='All Category'
+            style={{ width: 120 }}
+         >
+            <Option value='all'>All Category</Option>
+            <Option value='author'>Author</Option>
+            <Option value='name'>Books</Option>
+         </Select>
+         <div className='search__input'>
+            <Input placeholder='Basic usage' />
+         </div>
+         <div className='search__button'>
+            <SearchOutlined />
+         </div>
+      </div>
+   );
 }
 
 export default SearchForm;
