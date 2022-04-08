75013a27ad2ff1dc47a3bed3ec1c77c6fd37c7b6
ngvuq11 <ngvuquoc.17@gmail.com>
2022-04-08 16:55:14 +0700

    feat: 🎸 implement subcriber components

--- client/src/features/Home/pages/HomePage/index.jsx
+++ client/src/features/Home/pages/HomePage/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Container from '../../../../layouts/Container';
+import Subcriber from '../../components/Subcribers';
+import './HomePage.scss';
 
 function HomePage(props) {
    return (
       <div>
-         <h1>Hello HomePage</h1>
+         <p>Hello, I'm from HomePage</p>
+         <section></section>
+         <section className='section-subcriber'>
+            <p>Join the our Company</p>
+            <Subcriber />
+         </section>
       </div>
    );
 }
