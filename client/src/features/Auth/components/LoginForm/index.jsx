be1b470bf823fdc5c19716fc10acb645b9ef0d74
thanhnguyen662 <facebook662311@gmail.com>
2022-04-08 15:18:42 +0700

    feat: 🎸 login form interface

--- client/src/features/Auth/components/LoginForm/index.jsx
+++ client/src/features/Auth/components/LoginForm/index.jsx
@@ -1,13 +1,83 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import { Form, Input, Button, Checkbox, Typography } from 'antd';
+import { GoogleOutlined } from '@ant-design/icons';
+import './LoginForm.scss';
 
 LoginForm.propTypes = {};
 
 function LoginForm(props) {
+   const { Link, Text } = Typography;
+
+   const onFinish = (values) => {
+      console.log('Success:', values);
+   };
+
+   const onFinishFailed = (errorInfo) => {
+      console.log('Failed:', errorInfo);
+   };
+
    return (
-      <div>
-         <h1>This is a login form</h1>
-      </div>
+      <>
+         <Form
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete='off'
+            className='login-form'
+         >
+            <label className='login-form--label'>Username</label>
+            <Form.Item
+               className='login-form--inputWrap'
+               name='username'
+               rules={[
+                  { required: true, message: 'Please input your username!' },
+               ]}
+            >
+               <Input className='login-form--input' />
+            </Form.Item>
+
+            <label className='login-form--label'>Password</label>
+            <Form.Item
+               className='login-form--inputWrap'
+               name='password'
+               rules={[
+                  { required: true, message: 'Please input your password!' },
+               ]}
+            >
+               <Input.Password className='login-form--input' />
+            </Form.Item>
+
+            <div className='login-form--helper'>
+               <Form.Item name='remember' valuePropName='checked'>
+                  <Checkbox>Remember me</Checkbox>
+               </Form.Item>
+               <Link>Forgot Password</Link>
+            </div>
+
+            <Form.Item>
+               <Button
+                  type='primary'
+                  htmlType='submit'
+                  block
+                  className='login-form--submit'
+               >
+                  Sign in
+               </Button>
+               <Button
+                  className='login-form--google'
+                  block
+                  icon={<GoogleOutlined />}
+               >
+                  Sign in with Google
+               </Button>
+            </Form.Item>
+            <Form.Item className='login-form--signUp'>
+               <Text>Don't have a account? </Text>
+               <Link strong>Sign up for free!</Link>
+            </Form.Item>
+         </Form>
+      </>
    );
 }
 
--- client/src/features/Auth/pages/LoginPage/index.jsx
+++ client/src/features/Auth/pages/LoginPage/index.jsx
@@ -1,11 +1,26 @@
+import { Typography } from 'antd';
 import React from 'react';
 import LoginForm from '../../components/LoginForm';
+import './LoginPage.scss';
 
 function LoginPage(props) {
+   const { Text, Title } = Typography;
+
    return (
-      <div>
-         <h1>LoginPage</h1>
-         <LoginForm />
+      <div className='login-page'>
+         <div className='login-page--form'>
+            <div className='login-page--title'>
+               <Title>Welcome back</Title>
+               <Text>Welcome back! Please enter your details.</Text>
+            </div>
+            <LoginForm />
+         </div>
+         <div className='login-page--background'>
+            <img
+               src='https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+               className='login-page--image'
+            />
+         </div>
       </div>
    );
 }
