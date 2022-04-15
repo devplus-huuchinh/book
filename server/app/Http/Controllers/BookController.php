4b626a078baf61a95476d0e031625bd23c28c726
huuchinh <chinhthaihuu.devplus@gmail.com>
2022-04-15 10:09:08 +0700

    fix: 🐛 fix update condition

--- server/app/Http/Controllers/BookController.php
+++ server/app/Http/Controllers/BookController.php
@@ -9,12 +9,19 @@ class BookController extends Controller
 {
     public function store(Request $request)
     {
-        $user = $request->user();
-        $getUser = User::where('id', $user->id)->first();
-        if(Book::where('name', $request->name)->doesntExist() && $request->shareByUserId == $getUser->id){
-           $bookData = Book::create($request->all()); 
-           $bookData->save();
-           return response()->json($bookData);
+        try{
+            $user = $request->user();
+            $getUser = User::where('id', $user->id)->first();
+            if(Book::where('name', $request->name)->doesntExist() && $request->shareByUserId == $getUser->id){
+                $bookData = Book::create($request->all()); 
+                $bookData->save();
+                return response()->json($bookData);
+            }
+        }catch (\Throwable $error) {
+            return response()->json([
+                'message' => 'Addition_fail',
+                'error' => $error,
+            ]);
         }
     }
     public function show($id)
@@ -23,11 +30,20 @@ class BookController extends Controller
     }
     public function update(Request $request, $id)
     {
-        // $user = $request->user();
-        // $book= $request->book();
-        // if($book->shareByUserId == $user->id){
-            Book::where('id',$id)->update($request->all());
-        // }
+        try{
+            $user = $request->user();
+            $book= Book::where('shareByUserId',$user->id)->get();
+            //dd($user->id);
+            //dd($book->shareByUserId);
+            if($book->shareByUserId == $user->id){
+                Book::where('id',$id)->update($request->all());
+            }
+        }catch(\Throwable $error) {
+            return response()->json([
+                'message' => 'upadating_fail',
+                'error' => $error,
+            ]);
+        }
     }
     // public function updating(Request $request, $id){
     //     $book = Book::find($id)->get();
